34439cfa5c81d0e63a67c632c438e2db19fde781
alicansadeler <alicansadeler@gmail.com>
2024-09-26 21:57:59 +0300

    responsive güncellendi

--- src/components/headerComponents/CarouselHeader.jsx
+++ src/components/headerComponents/CarouselHeader.jsx
@@ -4,7 +4,7 @@ export const CarouselHeader = () => {
   const headerData = data.header.headerData;
   return (
     <>
-      <div className="bg-lineer-blue  flex flex-col items-center justify-around gap-10 m-2 rounded-[20px] mt-20 font-montserrat lg:mx-16 lg:mt-10 lg:flex-row lg:text-left lg:justify-around">
+      <div className="bg-lineer-blue  flex flex-col items-center justify-around gap-10 m-2 rounded-[20px] mt-20 font-montserrat lg:mx-16 lg:mt-10 lg:flex-row lg:text-left lg:justify-around lg:overflow-visible">
         <div className="px-8 space-y-8  lg:w-[420px]">
           <h2 className="mt-12  text-bgDarkBlue text-base font-bold ">
             {headerData.subtitle}
@@ -18,7 +18,11 @@ export const CarouselHeader = () => {
           </button>
         </div>
         <div>
-          <img src={"/col-md-6.png"} alt="" className="object-cover mt-16 " />
+          <img
+            src={"/col-md-6.png"}
+            alt=""
+            className="object-cover mt-16 lg:mt-0 lg:relative lg:left-16 2xl:left-48 2xl:h-[700px]"
+          />
         </div>
       </div>
     </>
